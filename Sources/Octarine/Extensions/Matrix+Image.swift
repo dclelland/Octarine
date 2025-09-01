30796f21e4f20587c86c9721e1cafad958293f76
Daniel Clelland <daniel.clelland@gmail.com>
2025-09-01 16:15:16 +0200

    Rename extensions

--- Sources/Octarine/Extensions/Matrix+Image.swift
+++ Sources/Octarine/Extensions/Matrix+Image.swift
@@ -1,5 +1,5 @@
 //
-//  Images.swift
+//  Matrix+Image.swift
 //  Images
 //
 //  Created by Daniel Clelland on 3/06/23.
@@ -14,7 +14,7 @@ import CoreImage
 import AppKit
 
 extension Matrix where Scalar == UInt8 {
-    
+
     public init?(nsImage: NSImage) {
         var rect = CGRect(x: 0, y: 0, width: nsImage.size.width, height: nsImage.size.height)
         guard let cgImage = nsImage.cgImage(forProposedRect: &rect, context: nil, hints: nil) else {
@@ -26,7 +26,7 @@ extension Matrix where Scalar == UInt8 {
     public var nsImage: NSImage {
         return NSImage(cgImage: cgImage, size: NSSize(width: shape.columns, height: shape.rows))
     }
-    
+
 }
 
 #elseif os(iOS)
@@ -34,7 +34,7 @@ extension Matrix where Scalar == UInt8 {
 import UIKit
 
 extension Matrix where Scalar == UInt8 {
-    
+
     public init?(uiImage: UIImage) {
         guard let cgImage = uiImage.cgImage else {
             return nil
@@ -45,56 +45,56 @@ extension Matrix where Scalar == UInt8 {
     public var uiImage: UIImage {
         return UIImage(cgImage: cgImage)
     }
-    
+
 }
 
 #endif
 
 extension Matrix where Scalar == UInt8 {
-    
+
     public init?(ciImage: CIImage) {
         guard let cgImage = CIContext().createCGImage(ciImage, from: ciImage.extent) else {
             return nil
         }
         self.init(cgImage: cgImage)
     }
-    
+
     public var ciImage: CIImage {
         return CIImage(cgImage: cgImage)
     }
-    
+
 }
 
 extension Matrix where Scalar == UInt8 {
-    
+
     public init?(cgImage: CGImage) {
         var cgImageFormat = Self.cgImageFormat
         try? self.init(pixelBuffer: vImage.PixelBuffer<vImage.Planar8>(cgImage: cgImage, cgImageFormat: &cgImageFormat))
     }
-    
+
     public var cgImage: CGImage {
         return pixelBuffer.makeCGImage(cgImageFormat: Self.cgImageFormat)!
     }
-    
+
     private static let cgImageFormat = vImage_CGImageFormat(
         bitsPerComponent: vImage.Planar8.bitCountPerComponent,
         bitsPerPixel: vImage.Planar8.bitCountPerPixel,
         colorSpace: CGColorSpaceCreateDeviceGray(),
         bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
     )!
-    
+
 }
 
 extension Matrix where Scalar == UInt8 {
-    
+
     public init(pixelBuffer: vImage.PixelBuffer<vImage.Planar8>) {
         self.init(shape: .init(rows: pixelBuffer.height, columns: pixelBuffer.width), elements: pixelBuffer.array)
     }
-    
+
     public var pixelBuffer: vImage.PixelBuffer<vImage.Planar8> {
         return vImage.PixelBuffer<vImage.Planar8>(pixelValues: elements, size: vImage.Size(width: shape.columns, height: shape.rows))
     }
-    
+
 }
 
 #if os(macOS)
@@ -102,7 +102,7 @@ extension Matrix where Scalar == UInt8 {
 import AppKit
 
 extension Matrix where Scalar == Float {
-    
+
     public init?(nsImage: NSImage) {
         var rect = CGRect(x: 0, y: 0, width: nsImage.size.width, height: nsImage.size.height)
         guard let cgImage = nsImage.cgImage(forProposedRect: &rect, context: nil, hints: nil) else {
@@ -114,7 +114,7 @@ extension Matrix where Scalar == Float {
     public var nsImage: NSImage {
         return NSImage(cgImage: cgImage, size: NSSize(width: shape.columns, height: shape.rows))
     }
-    
+
 }
 
 #elseif os(iOS)
@@ -122,7 +122,7 @@ extension Matrix where Scalar == Float {
 import UIKit
 
 extension Matrix where Scalar == Float {
-    
+
     public init?(uiImage: UIImage) {
         guard let cgImage = uiImage.cgImage else {
             return nil
@@ -133,54 +133,54 @@ extension Matrix where Scalar == Float {
     public var uiImage: UIImage {
         return UIImage(cgImage: cgImage)
     }
-    
+
 }
 
 #endif
 
 extension Matrix where Scalar == Float {
-    
+
     public init?(ciImage: CIImage) {
         guard let cgImage = CIContext().createCGImage(ciImage, from: ciImage.extent) else {
             return nil
         }
         self.init(cgImage: cgImage)
     }
-    
+
     public var ciImage: CIImage {
         return CIImage(cgImage: cgImage)
     }
-    
+
 }
 
 extension Matrix where Scalar == Float {
-    
+
     public init?(cgImage: CGImage) {
         var cgImageFormat = Self.cgImageFormat
         try? self.init(pixelBuffer: vImage.PixelBuffer<vImage.PlanarF>(cgImage: cgImage, cgImageFormat: &cgImageFormat))
     }
-    
+
     public var cgImage: CGImage {
         return pixelBuffer.makeCGImage(cgImageFormat: Self.cgImageFormat)!
     }
-    
+
     private static let cgImageFormat = vImage_CGImageFormat(
         bitsPerComponent: vImage.PlanarF.bitCountPerComponent,
         bitsPerPixel: vImage.PlanarF.bitCountPerPixel,
         colorSpace: CGColorSpaceCreateDeviceGray(),
         bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue | CGBitmapInfo.floatComponents.rawValue | CGBitmapInfo.byteOrder32Little.rawValue)
     )!
-    
+
 }
 
 extension Matrix where Scalar == Float {
-    
+
     public init(pixelBuffer: vImage.PixelBuffer<vImage.PlanarF>) {
         self.init(shape: .init(rows: pixelBuffer.height, columns: pixelBuffer.width), elements: pixelBuffer.array)
     }
-    
+
     public var pixelBuffer: vImage.PixelBuffer<vImage.PlanarF> {
         return vImage.PixelBuffer<vImage.PlanarF>(pixelValues: elements, size: vImage.Size(width: shape.columns, height: shape.rows))
     }
-    
+
 }
